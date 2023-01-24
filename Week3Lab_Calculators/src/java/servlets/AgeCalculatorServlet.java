cf4e23c5679ebb4729c160699ede69413938ee96
vitor <vitor@LAPTOP-QS24IM11>
2023-01-24 10:38:17 -0700

    Week3 Lab First version

---
+++ Week3Lab_Calculators/src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,53 @@
+
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author vitor
+ */
+public class AgeCalculatorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String age_string = request.getParameter("inputage");
+        
+        if (age_string.equals("")) {
+            request.setAttribute("displaymessage", "You must give your current age");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+            return;
+        }
+        
+        int age;
+        try {
+            age = (Integer.parseInt(age_string)) + 1;
+        } catch (NumberFormatException nfe) {
+           request.setAttribute("displaymessage", "You must enter a number");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+            return;
+        }
+        
+        String age_message = "Your age next birthday will be ";
+        request.setAttribute("displaymessage", age_message + age);
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+                .forward(request, response);
+        
+    }
+
+}
---
+++ Week3Lab_Calculators/web/WEB-INF/agecalculator.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : agecalculator.jsp
+    Created on : Jan 23, 2023, 11:12:48 PM
+    Author     : vitor
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Age Calculator</h1>
+        <form method="post" action="age">
+            Enter your age: <input type="text" name="inputage"><br>
+            <input type="submit" value="Age next birthday">
+        </form>
+        <p>${displaymessage}</p>
+        <p>Colocar o link</p>
+    </body>
+</html>
